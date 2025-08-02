dbc5c8cd3ad08791ba747fd2b8692ad8b0a1e19f
Kieran Klukas <me@dunkirk.sh>
2025-08-02 17:01:49 -0400

    chore: bump lix version

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
