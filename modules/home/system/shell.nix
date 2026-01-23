4d1069473d1126fc77d67601ea510a91110aa3bf
Kieran Klukas <me@dunkirk.sh>
2026-01-23 10:00:42 -0500

    feat: improve ghrpc with templates

--- modules/home/system/shell.nix
+++ modules/home/system/shell.nix
@@ -514,6 +514,23 @@ let
 
        ${pkgs.gum}/bin/gum style --foreground 212 --bold "Creating repository: $NAME"
 
+       # Prompt for templates
+       ADD_README=false
+       ADD_LICENSE=false
+       LICENSE_TYPE=""
+       if [[ -t 0 ]]; then
+         if ${pkgs.gum}/bin/gum confirm "Add templates (README/LICENSE)?"; then
+           TEMPLATES=$(${pkgs.gum}/bin/gum choose --no-limit --header "Select templates to add" "README.md" "LICENSE.md")
+           if echo "$TEMPLATES" | grep -q "README.md"; then
+             ADD_README=true
+           fi
+           if echo "$TEMPLATES" | grep -q "LICENSE.md"; then
+             ADD_LICENSE=true
+             LICENSE_TYPE=$(${pkgs.gum}/bin/gum choose --header "Select license" "MIT" "Apache-2.0" "GPL-3.0" "BSD-3-Clause" "ISC" "O'Saasy" "Unlicense")
+           fi
+         fi
+       fi
+
        # Create on Tangled
        if [[ "$TANGLED" == true ]]; then
          tangled_cookie=""
@@ -587,12 +604,188 @@ let
          fi
 
          # Set default push to origin (knot)
-         ${pkgs.git}/bin/git config branch.$BRANCH.remote origin 2>/dev/null || true
+          ${pkgs.git}/bin/git config branch.$BRANCH.remote origin 2>/dev/null || true
 
-         echo
-         ${pkgs.git}/bin/git remote -v
+           echo
+           ${pkgs.git}/bin/git remote -v
+         fi
+
+       # Add templates if requested
+       CURRENT_YEAR=$(date +%Y)
+       if [[ "$ADD_README" == true ]] && [[ ! -f "README.md" ]]; then
+         if [[ "$TANGLED" == true ]] && [[ "$GITHUB" == true ]]; then
+           cat > README.md <<'READMEEOF'
+# $NAME
+
+$DESCRIPTION
+
+The canonical repo for this is hosted on tangled over at [`dunkirk.sh/$NAME`](https://tangled.org/@dunkirk.sh/$NAME)
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/taciturnaxolotl/carriage/main/.github/images/line-break.svg" />
+</p>
+
+<p align="center">
+    <i><code>&copy $CURRENT_YEAR-present <a href="https://dunkirk.sh">Kieran Klukas</a></code></i>
+</p>
+
+<p align="center">
+    <a href="https://tangled.org/dunkirk.sh/$NAME/blob/main/LICENSE.md"><img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=$LICENSE_TYPE&logoColor=d9e0ee&colorA=363a4f&colorB=b7bdf8"/></a>
+</p>
+READMEEOF
+           ${pkgs.gnused}/bin/sed -i "s/\$NAME/$NAME/g; s/\$DESCRIPTION/$DESCRIPTION/g; s/\$CURRENT_YEAR/$CURRENT_YEAR/g; s/\$LICENSE_TYPE/$LICENSE_TYPE/g" README.md
+         elif [[ "$GITHUB" == true ]]; then
+           cat > README.md <<'READMEEOF'
+# $NAME
+
+$DESCRIPTION
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/taciturnaxolotl/carriage/main/.github/images/line-break.svg" />
+</p>
+
+<p align="center">
+    <i><code>&copy $CURRENT_YEAR-present <a href="https://dunkirk.sh">Kieran Klukas</a></code></i>
+</p>
+
+<p align="center">
+    <a href="https://github.com/$GITHUB_USER/$NAME/blob/main/LICENSE.md"><img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=$LICENSE_TYPE&logoColor=d9e0ee&colorA=363a4f&colorB=b7bdf8"/></a>
+</p>
+READMEEOF
+           ${pkgs.gnused}/bin/sed -i "s/\$NAME/$NAME/g; s/\$DESCRIPTION/$DESCRIPTION/g; s/\$CURRENT_YEAR/$CURRENT_YEAR/g; s/\$LICENSE_TYPE/$LICENSE_TYPE/g; s/\$GITHUB_USER/$GITHUB_USER/g" README.md
+         else
+           cat > README.md <<'READMEEOF'
+# $NAME
+
+$DESCRIPTION
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/taciturnaxolotl/carriage/main/.github/images/line-break.svg" />
+</p>
+
+<p align="center">
+    <i><code>&copy $CURRENT_YEAR-present <a href="https://dunkirk.sh">Kieran Klukas</a></code></i>
+</p>
+
+<p align="center">
+    <a href="https://tangled.org/dunkirk.sh/$NAME/blob/main/LICENSE.md"><img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=$LICENSE_TYPE&logoColor=d9e0ee&colorA=363a4f&colorB=b7bdf8"/></a>
+</p>
+READMEEOF
+           ${pkgs.gnused}/bin/sed -i "s/\$NAME/$NAME/g; s/\$DESCRIPTION/$DESCRIPTION/g; s/\$CURRENT_YEAR/$CURRENT_YEAR/g; s/\$LICENSE_TYPE/$LICENSE_TYPE/g" README.md
+         fi
+         ${pkgs.gum}/bin/gum style --foreground 35 "✓ Created README.md"
        fi
-  '';
+
+       if [[ "$ADD_LICENSE" == true ]] && [[ ! -f "LICENSE.md" ]]; then
+         case "$LICENSE_TYPE" in
+           "MIT")
+             cat > LICENSE.md <<LICENSEEOF
+# MIT License
+
+Copyright © \`$CURRENT_YEAR\` \`Kieran Klukas\`
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+LICENSEEOF
+             ;;
+           "Apache-2.0")
+             cat > LICENSE.md <<LICENSEEOF
+# Apache License 2.0
+
+Copyright © \`$CURRENT_YEAR\` \`Kieran Klukas\`
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+LICENSEEOF
+             ;;
+           "GPL-3.0")
+             cat > LICENSE.md <<LICENSEEOF
+# GNU General Public License v3.0
+
+Copyright © \`$CURRENT_YEAR\` \`Kieran Klukas\`
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+LICENSEEOF
+             ;;
+           "BSD-3-Clause")
+             cat > LICENSE.md <<LICENSEEOF
+# BSD 3-Clause License
+
+Copyright © \`$CURRENT_YEAR\` \`Kieran Klukas\`
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+LICENSEEOF
+             ;;
+           "ISC")
+             cat > LICENSE.md <<LICENSEEOF
+# ISC License
+
+Copyright © \`$CURRENT_YEAR\` \`Kieran Klukas\`
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+LICENSEEOF
+             ;;
+           "O'Saasy")
+             cat > LICENSE.md <<LICENSEEOF
+# The O'Saasy License
+
+Copyright © \`$CURRENT_YEAR\` \`Kieran Klukas\`
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+No licensee or downstream recipient may use the Software (including any modified or derivative versions) to directly compete with the original Licensor by offering it to third parties as a hosted, managed, or Software-as-a-Service (SaaS) product or cloud service where the primary value of the service is the functionality of the Software itself.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+LICENSEEOF
+             ;;
+           "Unlicense")
+             cat > LICENSE.md <<LICENSEEOF
+# The Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+LICENSEEOF
+             ;;
+         esac
+         ${pkgs.gum}/bin/gum style --foreground 35 "✓ Created LICENSE.md ($LICENSE_TYPE)"
+       fi
+
+       # Commit and push templates if created
+       if [[ "$ADD_README" == true ]] || [[ "$ADD_LICENSE" == true ]]; then
+         if ${pkgs.git}/bin/git rev-parse --is-inside-work-tree &>/dev/null; then
+           ${pkgs.git}/bin/git add README.md LICENSE.md 2>/dev/null || true
+           ${pkgs.git}/bin/git commit -m "Add project templates" 2>/dev/null || true
+           ${pkgs.git}/bin/git push origin $BRANCH 2>/dev/null || true
+           ${pkgs.git}/bin/git push github $BRANCH 2>/dev/null || true
+         fi
+       fi
+         '';
 
 in
 {
