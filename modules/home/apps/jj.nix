166e0cf7fe33bbbe23d0f2fc1848f7fc0db2f023
Kieran Klukas <kieran@dunkirk.sh>
2026-04-14 17:38:51 -0400

    feat: track bookmarks in jj

--- modules/home/apps/jj.nix
+++ modules/home/apps/jj.nix
@@ -28,10 +28,7 @@
           backend = "ssh";
           key = "~/.ssh/id_rsa.pub";
         };
-        git = {
-          push-new-bookmarks = true;
-          auto-local-bookmark = true;
-        };
+        remotes.origin.auto-track-bookmarks = "*";
         "revset-aliases" = {
           "mine()" = "author(kieran@dunkirk.sh) | author(me@dunkirk.sh)";
         };
