a40bf6de4ad4f4130493d76ce17ad8be0db07195
Kieran Klukas <kieran@dunkirk.sh>
2026-06-11 12:15:31 -0400

    feat: add sudo plugin

--- modules/home/system/shell.nix
+++ modules/home/system/shell.nix
@@ -1222,6 +1222,59 @@ in
                 zle -N copy-buffer-to-clipboard
                 bindkey '^X^C' copy-buffer-to-clipboard
 
+                # Double-tap escape to prepend sudo (from oh-my-zsh sudo plugin)
+                __sudo-replace-buffer() {
+                  local old=$1 new=$2 space=''${2:+ }
+                  if [[ $CURSOR -le ''${#old} ]]; then
+                    BUFFER="''${new}''${space}''${BUFFER#$old }"
+                    CURSOR=''${#new}
+                  else
+                    LBUFFER="''${new}''${space}''${LBUFFER#$old }"
+                  fi
+                }
+                sudo-command-line() {
+                  [[ -z $BUFFER ]] && LBUFFER="$(fc -ln -1)"
+                  local WHITESPACE=""
+                  if [[ ''${LBUFFER:0:1} = " " ]]; then
+                    WHITESPACE=" "
+                    LBUFFER="''${LBUFFER:1}"
+                  fi
+                  {
+                    local EDITOR=''${SUDO_EDITOR:-''${VISUAL:-$EDITOR}}
+                    if [[ -z "$EDITOR" ]]; then
+                      case "$BUFFER" in
+                        sudo\ -e\ *) __sudo-replace-buffer "sudo -e" "" ;;
+                        sudo\ *) __sudo-replace-buffer "sudo" "" ;;
+                        *) LBUFFER="sudo $LBUFFER" ;;
+                      esac
+                      return
+                    fi
+                    local cmd="''${''${(Az)BUFFER}[1]}"
+                    local realcmd="''${''${(Az)aliases[$cmd]}[1]:-$cmd}"
+                    local editorcmd="''${''${(Az)EDITOR}[1]}"
+                    if [[ "$realcmd" = (\$EDITOR|$editorcmd|''${editorcmd:c}) \
+                      || "''${realcmd:c}" = ($editorcmd|''${editorcmd:c}) ]] \
+                      || builtin which -a "$realcmd" | command grep -Fx -q "$editorcmd"; then
+                      __sudo-replace-buffer "$cmd" "sudo -e"
+                      return
+                    fi
+                    case "$BUFFER" in
+                      $editorcmd\ *) __sudo-replace-buffer "$editorcmd" "sudo -e" ;;
+                      \$EDITOR\ *) __sudo-replace-buffer '$EDITOR' "sudo -e" ;;
+                      sudo\ -e\ *) __sudo-replace-buffer "sudo -e" "$EDITOR" ;;
+                      sudo\ *) __sudo-replace-buffer "sudo" "" ;;
+                      *) LBUFFER="sudo $LBUFFER" ;;
+                    esac
+                  } always {
+                    LBUFFER="''${WHITESPACE}''${LBUFFER}"
+                    zle && zle redisplay
+                  }
+                }
+                zle -N sudo-command-line
+                bindkey -M emacs '\e\e' sudo-command-line
+                bindkey -M vicmd '\e\e' sudo-command-line
+                bindkey -M viins '\e\e' sudo-command-line
+
                 # chpwd hooks
                 autoload -Uz add-zsh-hook
 
